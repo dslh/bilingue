1435d2c945fd9941740c52b726c01a4f265fd9d0
Doug Hammond <d.lakehammond@gmail.com>
2012-03-14 01:16:06 +1300

    Removing custom tagging solution.

--- db/migrate/20120302113713_create_phrases_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreatePhrasesTags < ActiveRecord::Migration
-  def change
-    create_table :phrases_tags, :id => false do |t|
-      t.integer :phrase_id
-      t.integer :tag_id
-    end
-  end
-end
