178d807848e453ef114665de000cbb22dc137d2c
Doug Hammond <d.lakehammond@gmail.com>
2012-03-16 23:07:17 +1300

    Working around a bug in rocket_tag.
    rocket_tag's `tagged_with` method fails when running on top of PostgreSQL.
    See https://github.com/bradphelan/rocket_tag/issues/6

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,11 +1,24 @@
 class QuizzesController < ApplicationController
-  # GET /quizzes/tag+tag2/20
+  # GET /quizzes/language/1/tag+tag2/20
   def show
     @tags = params[:tags].split('+')
     @language = Language.find(params[:language])
-    @phrases = @language.phrases.tagged_with(@tags)
-    length = [params[:length].to_i || 10, @phrases.length].min
-    @phrases = @phrases.shuffle[0...length]
+# The below code is preferred but at present
+# there is an issue with rocket_tag + postgresql.
+#    @phrases = @language.phrases.tagged_with(@tags)
+#    length = [params[:length].to_i || 10, @phrases.length].min
+#    @phrases = @phrases.shuffle[0...length]
+    @phrases = Phrase.find_by_sql("
+      SELECT * FROM phrases WHERE #{@tags.length} = (
+        SELECT count(*) FROM taggings JOIN tags
+          ON tags.id = taggings.tag_id
+        WHERE tags.name IN (
+          #{@tags.collect { |t| "'#{t}'" }.join(',')}
+        ) AND taggings.taggable_id = phrases.id
+        AND taggings.taggable_type = 'Phrase'
+      ) AND language_id=#{@language.id}
+      ORDER BY random() LIMIT #{params[:length] || 10}
+    ")
 
     respond_to do |format|
       format.html { render 'quiz' }
