0480e2e782feee2abbf1952b1d5331b34d65705c
Doug Hammond <d.lakehammond@gmail.com>
2012-03-23 00:24:28 +1300

    Removed workaround for postgres issue.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,22 +3,7 @@ class QuizzesController < ApplicationController
   def show
     @tags = params[:tags].split('+')
     @language = Language.find(params[:language])
-# The below code is preferred but at present
-# there is an issue with rocket_tag + postgresql.
-#    @phrases = @language.phrases.tagged_with(@tags)
-#    length = [params[:length].to_i || 10, @phrases.length].min
-#    @phrases = @phrases.shuffle[0...length]
-    @phrases = Phrase.find_by_sql("
-      SELECT * FROM phrases WHERE #{@tags.length} = (
-        SELECT count(*) FROM taggings JOIN tags
-          ON tags.id = taggings.tag_id
-        WHERE tags.name IN (
-          #{@tags.collect { |t| "'#{t}'" }.join(',')}
-        ) AND taggings.taggable_id = phrases.id
-        AND taggings.taggable_type = 'Phrase'
-      ) AND language_id=#{@language.id}
-      ORDER BY random() LIMIT #{params[:length] || 10}
-    ")
+    @phrases = @language.phrases.tagged_with(@tags).limit(params[:length].to_i).shuffle
 
     respond_to do |format|
       format.html { render 'quiz' }
